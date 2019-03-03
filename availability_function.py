23756d9646117f44e2cca2cbb3b0dc661b1e3711
silly2Cans <davidgrght@gmail.com>
2019-03-03 00:15:45 +0000

    Created availability function and environment.yml file

---
+++ availability_function.py
@@ -0,0 +1,41 @@
+#! usr/bin/env python3
+
+import requests
+import json
+from pprint import pprint
+import sqlalchemy as sqla
+from sqlalchemy import create_engine
+import traceback
+import glob
+import os
+import simplejson as simjson
+import time
+from IPython.display import display
+import mysql
+import pymysql
+import time
+
+CONTRACT = 'Dublin'
+APIKEY = '7b9a350297fefef5f4147e65b6bc3114aacde014'
+STATIONS = 'https://api.jcdecaux.com/vls/v1/stations'
+DBURL = 'swen-db.cakbys7wmjxf.eu-west-1.rds.amazonaws.com'
+PORT = '3306'
+DB = 'dublin_bikes'
+USER = 'swen_main_'
+PASSWORD = 'mixednuts'
+
+def avail_to_db():
+    request = requests.get(STATIONS, params={'contract':CONTRACT,'apiKey':APIKEY})
+    availability = json.loads(request.text)
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, DBURL, PORT, DB))
+    for data in availability:
+        data_points = (data.get('number'), data.get('available_bikes'), data.get('available_bike_stands'), 
+                       data.get('last_update'), data.get('number')*data.get('last_update'))
+#         engine.execute("INSERT INTO availability_data values (%s,%s,%s,%s,%s)", data_points)
+        print(data_points)
+
+while True:
+    avail_to_db()
+    time.sleep(5)
+
+#avail_to_db()
