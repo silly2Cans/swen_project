664b5131a9ecb3856e35b130ade0171a6cf55cd0
ZhenqiYang <46993808+ZhenqiYang@users.noreply.github.com>
2019-03-04 12:54:34 +0000

    update changes
    update changes

--- db.py
+++ db.py
@@ -1,17 +1,16 @@
 # coding:utf-8
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import pymysql
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
-
 class Config(object):
-    
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/db_python04"
-
- 
+    ##this is our  database at aws ec2
+    ##SQLALCHEMY_DATABASE_URI = "mysql://swen_main_:mixednuts@swen-db.cakbys7wmjxf.eu-west-1.rds.amazonaws.com:3306/dublin_bikes"
+    ##this is my local database info
+    SQLALCHEMY_DATABASE_URI = "mysql://jeff:jeff003@localhost:3306/jack"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
@@ -22,11 +21,7 @@ db = SQLAlchemy(app)
 
 
 if __name__ == '__main__':
-    
-    
-
-    db.create_all()
-
+    app.run(debug=True)
 
 
 
