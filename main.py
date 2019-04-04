b785186610dd2118d44d4f25c1b3327fda3ca020
ZhenqiYang <46993808+ZhenqiYang@users.noreply.github.com>
2019-04-04 17:20:49 +0100

    Add files via upload
    update the goole maps of flask, 	created a basic scraper that fetches data from weatherfacasting, creates DB

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from flask import Flask, current_app,request,render_template,url_for
+from flask import Flask, current_app,request,render_template,url_for,jsonify,g
+from sqlalchemy import create_engine
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
+# import sys
+# print(sys.modules)
+# import sqlalchemy
+# [name for name in sys.modules if 'sqlalchemy' in name]
+
 
 app = Flask(__name__,
             static_url_path="/static",  
@@ -8,7 +18,6 @@ app = Flask(__name__,
 
 class Config(object):
     DEBUG = True
-    ITCAST = "python"
 
 
 app.config.from_object(Config)
@@ -27,6 +36,61 @@ def bike():
 @app.route("/googlemaps", methods=["GET","POST"])
 def maps():
     return render_template("googlemaps.html")
+
+
+
+def connect_to_database():
+    engine = create_engine("mysql+mysqldb://swen_main_:mixednuts@swen-db.cakbys7wmjxf.eu-west-1.rds.amazonaws.com:3306/dublin_bikes", echo=True)
+    return engine
+    # //engine ="mysql://swen_main_:mixednuts@swen-db.cakbys7wmjxf.eu-west-1.rds.amazonaws.com:3306/dublin_bikes"
+    # //return engine
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.dispose()
+
+@app.route("/stations", methods=["GET","POST"])
+def get_stations():
+    engine = get_db()
+    stations = []
+    rows = engine.execute("SELECT * from stations1;")
+    for row in rows:
+        stations.append(dict(row))
+    return jsonify(stations)
+    # print(len(stations))
+    # return stations
+    # for station in stations:
+    #     print(station)
+    # rows = engine.execute("SELECT * from stations1;")
+    # print('#found {} stations', len(rows))
+    # return jsonify(stations=[dict(row.items()) for row in rows])
+
+
+# @app.route("/stations", methods=["GET","POST"])
+# def get_stations():
+#     conn = pymysql.connect(host='swen-db.cakbys7wmjxf.eu-west-1.rds.amazonaws.com', port=3306, user='swen_main_', password='mixednuts', database='dublin_bikes')
+#     cur = conn.cursor()
+#     stations = []
+#     rows = cur.execute("SELECT * from stations1;")
+#     stations=cur.fetchall()[1]
+#     # print(len(rows))
+#     # sql = "select * from station;"
+#     # rows = engine.execute(sql).fetchall()
+#     # print('#found {} stations', len(rows))
+#     # return jsonify(stations=[dict(row.items()) for row in rows])
+#     cur.close()
+#     conn.close()
+#     # //return jsonify(stations=stations)
+#     return jsonify(stations=[stations])
+#
 
 if __name__ == '__main__':
     # app.run()
---
+++ weatherfacasting.py
@@ -0,0 +1,81 @@
+from pprint import pprint
+import requests
+import json
+import sqlalchemy as sqla
+from sqlalchemy import create_engine
+import traceback
+import glob
+import os
+import simplejson as simjson
+import time
+# from IPython.display import display
+import pymysql
+
+def get_forecast():
+    """Get the weather data from openweathermap.org API"""
+
+    APIURL = "http://api.openweathermap.org/data/2.5/forecast"
+    APIKEY = "2a2976ca391b03a9c1c9f1eba7680faa"
+    CITYID = "2964574"
+    #     Make request and print the data
+    request = requests.get(APIURL, params={'id': CITYID, 'APPID': APIKEY})
+    #     return request.text
+    #     print(request.text)
+    data = json.loads(request.text)
+    #     print(data["city"])
+    return data
+
+
+#     pprint(data)
+forecast = get_forecast()
+#pprint(forecast)
+list1=forecast['list']
+#print(list1)
+length=len(list1)
+def getdict(num):
+    main1=list1[num]['main']
+    weather1=list1[num]['weather'][0]
+    weather1['icon'] = weather1['icon'] + ".png"
+    wind1=list1[num]['wind']
+    wind1['windspeed'] = wind1['speed']
+    del wind1['speed']
+    main1.update(weather1)
+    main1.update(wind1)
+    main1['date']=list1[num]['dt_txt']
+    return main1
+
+# weather1=list1[2]['main']
+# wind1=list1[0]['wind']
+# wind1['windspeed'] = wind1['speed']
+# del wind1['speed']
+# weather1.update(wind1)
+
+
+DBURL = 'swen-db.cakbys7wmjxf.eu-west-1.rds.amazonaws.com'
+PORT = '3306'
+DB = 'dublin_bikes'
+USER = 'swen_main_'
+PASSWORD = 'mixednuts'
+i=0
+engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, DBURL, PORT, DB))
+print(engine)
+
+while(i<length):
+    data=getdict(i)
+    data_points = (data.get('id'),data.get('date'), data.get('temp'), data.get('humidity'),
+                   data.get('pressure'),data.get('description'), data.get('icon'), data.get('windspeed'))
+    engine.execute("INSERT INTO weatherforecasting values (%s,%s,%s,%s,%s,%s,%s,%s)", data_points)
+    #         print(data_points)
+    #         print("%s,%s,%s,%s,%s,%s" % data_points)
+    i = i + 1
+engine.dispose()
+
+
+
+
+
+
+
+
+
+
